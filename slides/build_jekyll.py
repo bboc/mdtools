9645b41854f150e4a78d5574d6d55822c091efe9
bboc <bernhard.bockelbrink@gmail.com>
2018-01-29 00:13:03 +0100

    moved all TODOs to taskpaper

--- slides/build_jekyll.py
+++ slides/build_jekyll.py
@@ -2,14 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 Compile and preprocess all files so that jekyll can build a static (github) page out of it.
-
-- inject patter number
-- build an index file with all patterns
-- build an index file for each group: index content: patterns + index.md
-- build introduction and appendix with complete content?
-- add glossary as separate file!!!
-
 """
+
 import codecs
 from functools import partial
 import os
@@ -45,13 +39,10 @@ class JekyllWriter(object):
 
     def build(self):
 
-        # TODO: add front matter
-        # TODO: add index file for each chapter
         index = read_config(self.args.index)
 
         # add all the chapters
         for cidx, chapter in enumerate(self.config[CHAPTER_ORDER]):
-            # TODO: add group indexes
             # self._build_chapter_index(cidx, chapter)
 
             for sidx, section in enumerate(self.config[CHAPTERS][chapter]):
@@ -59,15 +50,12 @@ class JekyllWriter(object):
                 target_path = os.path.join(self.target_folder, md_filename(section))
                 self._copy_file(source_path, target_path, make_headline_prefix(self.args, self.config, cidx + 1, sidx + 1))
 
-        # TODO: add appendix (skip glossary!!)
-        # TODO: add full index
         self._build_index(index)
-        # TODO: add full glossary
         with codecs.open(os.path.join(self.target_folder, md_filename("glossary")), 'w+', 'utf-8') as target:
             self.glossary_renderer.render(target.write)
 
     def _build_index(self, index):
-        print self.args.template
+        """Build site index with from template Already translation-aware."""
         with codecs.open(self.args.template, 'r', 'utf-8') as source:
             with codecs.open(os.path.join(self.target_folder, md_filename("index")), 'w+', 'utf-8') as target:
                 processor = mdp.MarkdownProcessor(source, filters=[
