97bfe3d8896ec2c86b24eb90fa7423dbe2a4c2a4
bboc <bernhard.bockelbrink@gmail.com>
2018-01-29 10:38:26 +0100

    added index datastructure
    provide easy access to translated section and chapter names

--- slides/index.py
+++ slides/index.py
@@ -3,6 +3,7 @@
 
 import yaml
 import codecs
+from collections import defaultdict
 from operator import itemgetter
 from string import Template
 from textwrap import dedent
@@ -36,6 +37,47 @@ def cmd_build_deckset_index(args):
     c = read_config(args.index_db)
     with codecs.open(args.target, 'a', 'utf-8') as target:
         deckset_alphabetical_index(c['patterns'], target)
+
+
+def read_index_db(index_file):
+    """Create read index from index.yaml and provide a sorted data structure:
+
+    index = {
+        'groups': [group1, group2, ] # ordered by group number
+        'patterns-by-group': { # used to create group index files
+            'group1-path': [ pattern1, pattern2] # ordered by pattern order in group
+        }
+        'groups-by-path': {  # used to build navigation
+            'group-path': group,
+        }
+        'groups-by-gid': {
+            gid: group
+        }
+    }
+    groups and patterns are simple dictionaries.
+    """
+
+    index = read_config(index_file)
+
+    index['groups'].sort(key=itemgetter('gid'))
+
+    index['groups-by-gid'] = {}
+    index['group-by-path'] = {}
+    for group in index['groups']:
+        index['group-by-path'][group['path']] = group
+        index['groups-by-gid'][group['gid']] = group
+
+    # build patterns by group
+    index['patterns-by-group'] = defaultdict(list)
+
+    for pattern in index['patterns']:
+        gp = index['groups-by-gid'][pattern['gid']]['path']
+        index['patterns-by-group'][gp].append(pattern)
+
+    for gp in index['group-by-path'].keys():
+        index['patterns-by-group'][gp].sort(key=itemgetter('pid'))
+
+    return index
 
 
 def make_cell(items):
