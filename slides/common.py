f3e59c63513d01f89eee3b86b6fec7c09297ca6d
bboc <bernhard.bockelbrink@gmail.com>
2018-08-16 20:52:29 +0200

    parsing simple format is complete now

--- slides/common.py
+++ slides/common.py
@@ -67,7 +67,7 @@ class Content(object):
         if CONTENT in data:
             # parse new config format
             c.introduction = Part.from_config(data[CONTENT][FRONT_MATTER], FRONT_MATTER)
-            c.chapters = [Chapter.from_config(chapter, idx) for idx, chapter in enumerate(data[CONTENT][CHAPTERS], 1)]
+            c.chapters = [Chapter.from_config(chapter, id=idx) for idx, chapter in enumerate(data[CONTENT][CHAPTERS], 1)]
             c.appendix = Part.from_config(data[CONTENT][APPENDIX], APPENDIX)
 
             c.title = data[CONTENT].get(TITLE)
@@ -76,7 +76,7 @@ class Content(object):
         else:
             # parse old config format
             c.introduction = Part.from_config(data[FRONT_MATTER], FRONT_MATTER)
-            c.chapters = [Chapter.from_config(data[CHAPTERS][chapter_name], idx, chapter_name) for idx, chapter_name in enumerate(data[CHAPTER_ORDER], 1)]
+            c.chapters = [Chapter.from_config(data[CHAPTERS][chapter_name], id=idx, name=chapter_name) for idx, chapter_name in enumerate(data[CHAPTER_ORDER], 1)]
             c.appendix = Part.from_config(data[APPENDIX], APPENDIX)
 
             c.title = data.get(TITLE)
@@ -129,13 +129,15 @@ class Part(ContentItem):
         }
         or [section1, section2, …]
         """
+        part, section_source = cls._make_basic_item_from_config(data, name)
+        part.sections = [Section.from_config(s, sidx) for sidx, s in enumerate(section_source, 1)]
+        return part
 
-        return cls("Foo Bar", 'foo-bar', [])
-        import pprint
-        pprint.pprint(data)
-        # set defaults:
-        title = make_title(name)
-        slug = make_pathname(name)
+    @classmethod
+    def _make_basic_item_from_config(cls, data, name=None):
+        if name:
+            title = make_title(name)
+            slug = make_pathname(name)
         if type(data) == dict:
             if TITLE in data:
                 title = data[TITLE]
@@ -144,11 +146,14 @@ class Part(ContentItem):
             section_source = data[SECTIONS]
         elif type(data) == list:
             section_source = data
-        sections = [Section.from_config_data(s, idx, sidx) for sidx, s in enumerate(section_source, 1)]
-        return cls(title, slug, sections)
-   
-        if idx:
-            new_item[ID] = idx
+        return cls(title, slug), section_source
+
+    def to_dict(self):
+        d = super(Part, self).to_dict()
+        d.update({
+            SECTIONS: [s.to_dict() for s in self.sections],
+        })
+        return d
 
 
 class Chapter(Part):
@@ -157,8 +162,11 @@ class Chapter(Part):
         self.id = id
 
     @classmethod
-    def from_config(cls, data, id, name):
-        return super(Chapter, cls).from_config(data, name)
+    def from_config(cls, data, name=None, id=None):
+        chapter, section_source = super(Chapter, cls)._make_basic_item_from_config(data, name)
+        chapter.id = id
+        chapter.sections = [Section.from_config(s, sidx, chapter.id) for sidx, s in enumerate(section_source, 1)]
+        return chapter
 
     def to_dict(self):
         d = super(Chapter, self).to_dict()
@@ -176,21 +184,20 @@ class Section(ContentItem):
         self.chapter_id = chapter_id
 
     @classmethod
-    def from_config(cls, item, chapter_id=None, id=None):
+    def from_config(cls, item, id=None, chapter_id=None):
         if type(item) == str:
             title = make_title(item)
             slug = make_pathname(item)
         else:
             title = item[TITLE]
             slug = item[SLUG]
-        return cls(title, slug)
+        return cls(title, slug, id, chapter_id)
 
     def to_dict(self):
         d = super(Section, self).to_dict()
-        d.update({
-            ID: self.id,
-            CHAPTER_ID: self.chapter_id,
-        })
+        d[ID] = self.id
+        if self.chapter_id is not None:
+            d[CHAPTER_ID] = self.chapter_id
         return d
 
 
@@ -201,7 +208,6 @@ def parse_config_new(data):
     Parse raw config data structure into efficient in-memory structure.
 
     returns a dictionary with the config as a dictionary, that contains a Content object at index 'content'
-
     """
 
     def parse_chapter(item, idx):
--- tests/mdslides/config_tests.py
+++ tests/mdslides/config_tests.py
@@ -1,229 +1,28 @@
 # -*- coding: utf-8 -*-
 """
 Tests for reading the config and building in-memory objects
-
 """
 
 import yaml
-from textwrap import dedent
 import unittest
 
-from slides.common import parse_config, parse_config_new, Section, Content
+from slides.common import parse_config, Section, Content
+from config_test_data import result, simple_format, extended_format
 
 
 class ConfigTests(unittest.TestCase):
 
-    result = {
-        'content': {'appendix': {'sections': [{'id': 1,
-                                               'slug': 'glossary',
-                                               'title': 'Glossary'},
-                                              {'id': 2,
-                                               'slug': 'changelog',
-                                               'title': 'Changelog'},
-                                              {'id': 3,
-                                               'slug': 'license',
-                                               'title': 'License'}],
-                                 'slug': 'appendix',
-                                 'title': 'Appendix'},
-                    'chapters': [{'id': 1,
-                                  'sections': [{'chapter_id': 1,
-                                                'id': 1,
-                                                'slug': 'deckset',
-                                                'title': 'Deckset'},
-                                               {'chapter_id': 1,
-                                                'id': 2,
-                                                'slug': 'jekyll',
-                                                'title': 'Jekyll'}],
-                                  'slug': 'formats',
-                                  'title': 'Formats'},
-                                 {'id': 2,
-                                  'sections': [{'chapter_id': 2,
-                                                'id': 1,
-                                                'slug': 'glossary-entries',
-                                                'title': 'Glossary Entries'},
-                                               {'chapter_id': 2,
-                                                'id': 2,
-                                                'slug': 'section-links',
-                                                'title': 'Section Links'},
-                                               {'chapter_id': 2,
-                                                'id': 3,
-                                                'slug': 'chapter-headers',
-                                                'title': 'Chapter Headers'},
-                                               {'chapter_id': 2,
-                                                'id': 4,
-                                                'slug': 'index-files',
-                                                'title': 'Index Files'}],
-                                  'slug': 'features',
-                                  'title': 'Features'},
-                                 {'id': 3,
-                                  'sections': [{'chapter_id': 3,
-                                                'id': 1,
-                                                'slug': 'templates',
-                                                'title': 'Templates'}],
-                                  'slug': 'data-structure',
-                                  'title': 'Data Structure'},
-                                 {'id': 4,
-                                  'sections': [{'chapter_id': 4,
-                                                'id': 1,
-                                                'slug': 'mdslides',
-                                                'title': 'Mdslides'},
-                                               {'chapter_id': 4,
-                                                'id': 2,
-                                                'slug': 'mdimg',
-                                                'title': 'Mdimg'}],
-                                  'slug': 'commands',
-                                  'title': 'Commands'},
-                                 {'id': 5,
-                                  'sections': [{'chapter_id': 5,
-                                                'id': 1,
-                                                'slug': 'translating-templates',
-                                                'title': 'Translating Templates'}],
-                                  'slug': 'translation',
-                                  'title': 'Translation'}],
-                    'end': 'SKIP',
-                    'introduction': {'sections': [{'id': 1,
-                                                   'slug': 'overview',
-                                                   'title': 'Overview'},
-                                                  {'id': 2,
-                                                   'slug': 'usecases',
-                                                   'title': 'Usecases'}],
-                                     'slug': 'introduction',
-                                     'title': 'Introduction'},
-                    'title': 'title'},
-        'index': [{'chapter_id': 2,
-                   'id': 3,
-                   'slug': 'chapter-headers',
-                           'title': 'Chapter Headers'},
-                  {'chapter_id': 1,
-                   'id': 1,
-                   'slug': 'deckset',
-                           'title': 'Deckset'},
-                  {'chapter_id': 2,
-                   'id': 1,
-                   'slug': 'glossary-entries',
-                           'title': 'Glossary Entries'},
-                  {'chapter_id': 2,
-                   'id': 4,
-                   'slug': 'index-files',
-                           'title': 'Index Files'},
-                  {'chapter_id': 1,
-                   'id': 2,
-                   'slug': 'jekyll',
-                           'title': 'Jekyll'},
-                  {'chapter_id': 4,
-                   'id': 2,
-                   'slug': 'mdimg',
-                           'title': 'Mdimg'},
-                  {'chapter_id': 4,
-                   'id': 1,
-                   'slug': 'mdslides',
-                           'title': 'Mdslides'},
-                  {'chapter_id': 2,
-                   'id': 2,
-                   'slug': 'section-links',
-                           'title': 'Section Links'},
-                  {'chapter_id': 3,
-                   'id': 1,
-                   'slug': 'templates',
-                           'title': 'Templates'},
-                  {'chapter_id': 5,
-                   'id': 1,
-                   'slug': 'translating-templates',
-                           'title': 'Translating Templates'}],
-        'section-prefix': '%(chapter)s.%(section)s:'}
-
     def test_new_format(self):
         """Config in new format is properly parsed"""
         self.maxDiff = None
-        cfg = dedent("""
+        parsed = parse_config(yaml.load(extended_format))
 
-            section-prefix: "%(chapter)s.%(section)s:"
-            content:
-                title: title
-                end: SKIP
-                introduction:
-                    title: Introduction
-                    slug: introduction
-                    sections:
-                        - overview
-                        - usecases
-                chapters:
-                    - formats:
-                        - deckset
-                        - jekyll
-                    - features:
-                        - title: Glossary Entries
-                          slug: glossary-entries
-                        - section links
-                        - chapter headers
-                        - index files
-                    - title: Data Structure
-                      slug: data-structure
-                      sections:
-                        - title: Templates
-                          slug: templates
-                    - commands:
-                        - mdslides
-                        - mdimg
-                    - translation:
-                        - translating templates
-                appendix:
-                    - glossary
-                    - changelog
-                    - license
-         """)
-        parsed = parse_config(yaml.load(cfg))
-        import pprint
-        pprint.pprint(parsed)
-        pprint.pprint(self.result)
-        self.failUnlessEqual(parsed, self.result)
+        self.failUnlessEqual(parsed, result)
 
     def test_old_format(self):
         self.maxDiff = None
-        cfg = dedent("""
-            title: title
-            end: SKIP
-
-            section-prefix: "%(chapter)s.%(section)s:"
-
-            introduction:
-                - overview
-                - usecases
-
-            appendix:
-                - glossary
-                - changelog
-                - license
-
-            chapter-order:
-                - formats
-                - features
-                - data structure
-                - commands
-                - translation
-
-            chapters:
-                formats:
-                    - deckset
-                    - jekyll
-                features:
-                    - glossary entries
-                    - section links
-                    - chapter headers
-                    - index files
-                data structure:
-                    - templates
-                commands:
-                    - mdslides
-                    - mdimg
-                translation:
-                    - translating templates
-        """)
-        parsed = parse_config(yaml.load(cfg))
-        import pprint
-        pprint.pprint(parsed)
-        pprint.pprint(self.result)
-        self.failUnlessEqual(parsed, self.result)
+        parsed = parse_config(yaml.load(simple_format))
+        self.failUnlessEqual(parsed, result)
 
 
 class SectionTests(unittest.TestCase):
@@ -235,8 +34,7 @@ class SectionTests(unittest.TestCase):
         self.failUnlessEqual(s.to_dict(), {
             'title': 'Some Section Name',
             'slug': 'some-section-name',
-            'id': None,
-            'chapter_id': None})
+            'id': None})
 
     def test_new_style_section(self):
 
@@ -247,116 +45,19 @@ class SectionTests(unittest.TestCase):
         self.failUnlessEqual(s.to_dict(), {
             'title': 'Some Section Name',
             'slug': 'some-section-name',
-            'id': None,
-            'chapter_id': None})
+            'id': None})
 
-class PartTests(unittest.TestCase):
-    pass
-
-
-
-
-class ChapterTests(unittest.TestCase):
-    def test_new_style_chapter(self):
-
-        data = parse_config_new(yaml.load("""
-            - formats:
-                - deckset
-                - jekyll
-            - features:
-                - title: Glossary Entries
-                  slug: glossary-entries
-                - section links
-                - chapter headers
-                - index files
-            - title: Data Structure
-              slug: data-structure
-              sections:
-                - title: Templates
-                  slug: templates
-            - commands:
-                - mdslides
-                - mdimg
-            - translation:
-                - translating templates
-            """))
-
-
-
-class InheritaceTests(unittest.TestCase):
-
-    def test_it(self):
-
-        class Base(object):
-
-            @classmethod
-            def foo(cls):
-                print 'base'
-                return cls()
-
-        class Child(Base):
-            @classmethod
-            def foo(cls):
-                print 'chi;ld'
-                return super(Child, cls).foo()
-        base = Base.foo()
-        child = Child.foo()
-
-        print 'basetype', type(base)
-        print 'childtype', type(child)
-        self.failUnless(isinstance(base, Base))
-        self.failUnless(isinstance(child, Base))
-
-        self.failUnless(isinstance(child, Child))
 
 class ContentTests(unittest.TestCase):
 
     def test_old_format(self):
         self.maxDiff = None
-        cfg = dedent("""
-            title: title
-            end: SKIP
+        c = Content.from_config(yaml.load(simple_format))
+        self.failUnlessEqual(c.to_dict(), result['content'])
 
-            section-prefix: "%(chapter)s.%(section)s:"
+    def test_new_format(self):
+        """Config in new format is properly parsed"""
+        self.maxDiff = None
 
-            introduction:
-                - overview
-                - usecases
-
-            appendix:
-                - glossary
-                - changelog
-                - license
-
-            chapter-order:
-                - formats
-                - features
-                - data structure
-                - commands
-                - translation
-
-            chapters:
-                formats:
-                    - deckset
-                    - jekyll
-                features:
-                    - glossary entries
-                    - section links
-                    - chapter headers
-                    - index files
-                data structure:
-                    - templates
-                commands:
-                    - mdslides
-                    - mdimg
-                translation:
-                    - translating templates
-        """)
-        c = Content.from_config(yaml.load(cfg))
-        import pprint
-        pprint.pprint(c.to_dict())
-        pprint.pprint(self.result)
-        self.failUnlessEqual(c.to_dict(), self.result)
-
-
-
+        c = Content.from_config(yaml.load(extended_format))
+        self.failUnlessEqual(c.to_dict(), result['content'])
