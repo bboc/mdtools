f7e53b33c01a96a9e380e254887d19f0234ca474
bboc <bernhard.bockelbrink@gmail.com>
2021-01-17 22:31:08 +0100

    content object now reads new structure

---
+++ tests/config/structure_tests.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from slides.config import Content
+
+
+class TestStructure(unittest.TestCase):
+
+    def test_simple_structure(self):
+        c = Content.from_config({
+            'config': {},
+            'parts': [
+                {
+                    'id': 'introduction',
+                },
+                {
+                    'id': 'part 2',
+                    'chapters': [
+                        {
+                            "id": "chapter 1",
+                            "tags": ['foo', 'bar'],
+                            'sections': [
+                                {
+                                    'id': 'section 1',
+                                    'tags': ['foo'],
+                                },
+                                {
+                                    'id': 'section 2',
+                                    'config': {
+                                        'param1': 'value1',
+                                        'param2': 'value2',
+                                    }
+                                },
+                            ],
+                        },
+                        {
+                            "id": "chapter 2",
+                        },
+                    ],
+                },
+                {
+                    'id': 'appendix',
+                    'chapters': [
+                        {
+                            "id": "chapter 1",
+                        },
+                        {
+                            "id": "chapter 2",
+                        },
+                    ],
+                },
+            ]}, '/')
+        self.fail(c.to_dict())
