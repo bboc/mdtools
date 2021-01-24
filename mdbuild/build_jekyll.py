2933c3e7458be615bbb6b6e113496519e2d07c67
bboc <bernhard.bockelbrink@gmail.com>
2021-01-24 20:53:33 +0100

    moved index-macro to macros package

--- mdbuild/build_jekyll.py
+++ mdbuild/build_jekyll.py
@@ -40,84 +40,6 @@ def nav_el(target, template, item):
     target.write(template % dict(name=item.title, path=item.slug))
 
 
-class IndexMacro(object):
-    """
-    Process index macros.
-
-    old code for index
-    partial(mdp.insert_index, '<!-- GROUP-INDEX -->', self.config[CONTENT].chapters),
-    partial(mdp.insert_index, '<!-- PATTERN-INDEX -->', self.config[CONTENT].index, summary_db=self.summary_db, format='html', sort=True),
-    """
-
-    @classmethod
-    def render(cls, structure, format, *args, **kwargs):
-        """Create a (sorted) index of pages.
-
-        {{index:tag=pattern,sort=title}} create an index for all entries tagged 'pattern'
-        {{index:root=slug}} create an index of all children of node
-        sort and format default to None.
-        root is processed before tag filter.
-
-        # TODO: the structure module might be a better place for this
-        """
-        # get arguments
-        tag_filter = kwargs.get('tag')
-        sort = kwargs.get('sort')
-        root = structure
-        if 'root' in kwargs:
-            root = structure.find(kwargs['root'])
-            if not root:
-                print('WARNING: could not resolve item: {{index:root=', kwargs['root'], '}}')
-                return "{{index:root=%s ERRROR UNKNOWN ROOT}}" % kwargs['root']
-
-        # select which nodes to show
-        nodes_to_show = []
-        if tag_filter:
-            current_node = root
-            while current_node:
-                if tag_filter in current_node.tags:
-                    nodes_to_show.append(current_node)
-                current_node = current_node.successor
-        else:
-            nodes_to_show = root.children[:]
-
-        if sort:
-            nodes_to_show.sort(key=attrgetter(sort))
-
-        if format == 'html':
-            return cls.render_html(nodes_to_show)
-        else:  # plain list
-            return cls.render_plain(nodes_to_show)
-
-    INDEX_ELEMENT_PLAIN = "- [%(title)s](%(path)s.html)\n"
-
-    @classmethod
-    def render_plain(cls, nodes):
-        res = []
-        for node in nodes:
-            res.append(cls.INDEX_ELEMENT_PLAIN % dict(title=node.title, path=node.slug))
-        return '\n'.join(res)
-
-    @classmethod
-    def render_html(cls, nodes):
-        res = ["<dl>"]
-        for node in nodes:
-            res.append(cls.html_index_element(node.title, node.slug, node.summary))
-        res.append("</dl>")
-        return '\n'.join(res)
-
-    # TODO: enventually use dedent, but it messes up the diffs while the rewrite is in progress
-    INDEX_ELEMENT_HTML = """
-  <dt><a href="%(path)s.html">%(title)s</a></dt>
-  <dd>%(summary)s</dd>"""
-
-    @classmethod
-    def html_index_element(cls, title, path, summary):
-        if summary:
-            summary = markdown2html(summary)
-        else:
-            summary = ''
-        return cls.INDEX_ELEMENT_HTML % locals()
 
 
 class JekyllWriter(object):
@@ -133,7 +55,7 @@ class JekyllWriter(object):
 
         # register all macros here
         macros.register_macro('full-glossary', partial(glossary.glossary_macro, glossary.JekyllGlossaryRenderer(9999)))
-        macros.register_macro('index', partial(IndexMacro.render, self.structure, 'html'))
+        macros.register_macro('index', partial(macros.IndexMacro.render, self.structure, 'html'))
 
     def build(self):
         """Render the jekyll output.
--- mdbuild/macros/__init__.py
+++ mdbuild/macros/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from .core import register_macro, process_macro, MacroFilter
+
+from .index import IndexMacro
