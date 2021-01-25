935ee3d40de0b571b632a9976a8bce0463a3e156
bboc <bernhard.bockelbrink@gmail.com>
2021-01-25 22:47:47 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-
+# TODO: make a nicer setup.py from https://github.com/navdeep-G/setup.py
 setup(
     name="mdtools",
-    packages=find_packages(),
+    version='2.0.0',
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     install_requires=['polib'],
     author="Bernhard Bockelbrink",
     author_email="bernhard.bockelbrink@gmail.com",
     description="A commandline tools for publishing various document formats (Jekyll, LaTeX, ePub etc.) from a single markdown source.",
     long_description=read("README.md"),
-    keywords="Markdown reveal.js slide-decks i18n LaTeX EPUB Multimarkdown Jekyll",
+    keywords="single-source-publishing Markdown reveal.js slide-decks i18n LaTeX ePub Multimarkdown Jekyll GitHub-pages",
     url="https://github.com/bboc/mdtools",
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
