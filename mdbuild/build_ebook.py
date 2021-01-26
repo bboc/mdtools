b7cba06c4ac274f989783cb138cc4e4e35011919
bboc <bernhard.bockelbrink@gmail.com>
2021-01-26 22:25:02 +0100

    removed obsolete code

--- mdbuild/build_ebook.py
+++ mdbuild/build_ebook.py
@@ -50,38 +50,6 @@ class EbookWriter(object):
                 self._append_content(target, current_node)
                 current_node = current_node.successor
 
-    def build_old(self):
-        """Build three files: intro/patterns/appendix."""
-        content = self.config[CONTENT]
-        # build introduction
-
-        def build_intro_and_appendix(filename, part):
-            target_path = os.path.join(self.target_folder, filename)
-            with codecs.open(target_path, 'w+', 'utf-8') as target:
-                for item in part.sections:
-                    self._append_section(target, part, item)
-
-        build_intro_and_appendix('tmp-introduction.md', content.introduction)
-
-        # build all the chapters/sections
-        target_path = os.path.join(self.target_folder, 'tmp-chapters.md')
-        with codecs.open(target_path, 'a', 'utf-8') as target:
-            for chapter in content.chapters:
-                self._build_chapter_index(target, chapter)
-                for section in chapter.sections:
-                    self._append_section(target,
-                                         chapter,
-                                         section,
-                                         headline_level_increase=1,
-                                         headline_prefix=make_headline_prefix(self.args, self.config, chapter.id, section.id))
-
-        # finally build appendix
-        build_intro_and_appendix('tmp-appendix.md', content.appendix)
-
-        # emit glossary stuff into tmp-appendix.md if necessary
-        with codecs.open(os.path.join(self.target_folder, 'tmp-appendix.md'), 'a', 'utf-8') as target:
-            self.gp.glossary_post_processing(target)
-
     def _append_content(self, target, node, headline_level_increase=0, headline_prefix=None):
         """Append each section to target."""
         with codecs.open(node.source_path, 'r', 'utf-8') as source:
@@ -101,20 +69,3 @@ class EbookWriter(object):
             ])
             processor.process()
         target.write("\n\n")
-
-    def _build_chapter_index(self, target, chapter):
-        """Add chapter headline, and index.md if present."""
-
-        target.write('\n')
-        target.write('## %s \n' % chapter.title)
-        target.write('\n')
-        # this image is not rendered directly under the headline, so it makes no sense to have this
-        # target.write('\n![](img/pattern-groups/group-%s.png)\n\n' % chapter.id)
-        chapter_index_file = os.path.join(self.source_folder, chapter.slug, 'index.md')
-        if os.path.exists(chapter_index_file):
-            with codecs.open(chapter_index_file, 'r', 'utf-8') as cif:
-                next(cif)  # skip headline
-                processor = mdp.MarkdownProcessor(cif, filters=self.common_filters())
-                processor.add_filter(partial(mdp.write, target))
-                processor.process()
-            target.write('\n\n')
