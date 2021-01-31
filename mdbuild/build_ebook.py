e777ff9264c93cab91fec39da0e4e2ecc6f1dfbb
bboc <bernhard.bockelbrink@gmail.com>
2021-01-31 15:34:44 +0100

    bugfix

--- mdbuild/build_ebook.py
+++ mdbuild/build_ebook.py
@@ -39,7 +39,7 @@ class EbookWriter(object):
             mdp.clean_images,
         ]
         # process glossary links
-        if config.cfg.target_format in ['html', 'epub']:
+        if config.cfg.target_format == 'html':
             style = 'tooltip'
         else:
             style = 'plain'
